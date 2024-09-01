dead6b72dbc595df7143bdd032de1d99e0ffa7fd
chibabe97 <chidinmaojukwu321@gmail.com>
2024-09-01 17:43:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//var generateName = require('sillyname');
+
+
+//import generateName from "sillyname";
+//var sillyName = generateName();
+
+// import superheroes from "superheroes";
+import {randomSuperhero} from 'superheroes';
+const name = randomSuperhero();
+console.log(`My random superhero name is ${name}.`);
+// console.log(`My superheroes ${superheroes}.`);
